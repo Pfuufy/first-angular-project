aeaaa1ad3d6da7cb37d07e84898496af3669d721
Gabriel Bissey <gabrielbissey@gmail.com>
2018-11-29 11:34:15 -0700

    Adding first files

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+    serverId = 10;
+    serverStatus = '';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline'
+    }
+
+    getServerStatus() {
+        return this.serverStatus
+    }
+
+    getBackgroundColor() {
+        return this.serverStatus === 'Online' ? 'palegreen' : 'mistyrose'
+    }
+
+    getBorderColor() {
+        return this.serverStatus === 'Online' ? 'green' : 'red'
+    }
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,38 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test server';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! The name is ' + this.serverName;
+    console.log(this.servers);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
